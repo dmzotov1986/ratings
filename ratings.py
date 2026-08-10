d18ccacfd0dcb44864f2be610023250a718cd11c
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-10 03:51:46 +0300

    wip but class ready

--- ratings.py
+++ ratings.py
@@ -1,22 +1,24 @@
 class Rating:
-	def __init__(self, rating = 0.0):
-		self._rating = rating
 	@property
 	def rating(self):
 		return self._rating
 	def update(self, other, result):
 		if result < 0 or result > 2:
 			raise ValueError("Результат от 0 до 2")
-		increase = result - 2 / (10 ** ((other.rating - self.rating) / 25) + 1)
+		self._update(other, result)
+	def won(self, other):
+		self._update(other, 2)
+	def draw(self, other):
+		self._update(other, 1)
+	def lost(self, other):
+		self._update(other, 0)
+	def _update(self, other, result):
+		increase = result - 2 / (10 ** ((other._rating - self._rating) / 25) + 1)
 		self._rating += increase
 		other._rating -= increase
-	def won(self, other):
-		self.update(other, 2)
-	def draw(self, other):
-		self.update(other, 1)
-	def lost(self, other):
-		self.update(other, 0)
+	def __init__(self, rating = 0.0):
+		self._rating = rating
 inputs = list(map(float, input("Введите рейтинги двух игроков и результат матча (количество набранных очков первым игроком - 0, 1, 2): ").split()))
 first, second = ratings = [Rating(i) for i in inputs[:-1]]
 first.update(second, inputs[2])
-print("Рейтинги игроков:", [r.rating for r in ratings])
+print("Рейтинги игроков:", " ".join([str(r.rating) for r in ratings]))
