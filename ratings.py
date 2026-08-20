fd2d415d6d82ce1a2763b422191bee74a0db1381
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-21 02:41:57 +0300

    WIP с некоторыми ошибками.

--- ratings.py
+++ ratings.py
@@ -1,18 +1,13 @@
 class Rating:
 	def __init__(self, rating = 0.0):
 		self.rating = rating
-	def won(self, other):
-		self.update(other, 2)
-	def draw(self, other):
-		self.update(other, 1)
-	def lost(self, other):
-		self.update(other, 0)
 	def update(self, other, result):
 		increase = result - 2 / (10 ** ((other.rating - self.rating) / 25) + 1)
 		self.rating += increase
 		other.rating -= increase
 class Draw:
 	def __init__(self, n):
+		#Нечетные неправильно работают, нужен ещё тур! Надёжнее лишний игрок, с которым отдыхают.
 		self.n = n
 		self.tour = tuple((p, n + 1 - p) for p in range(1, (n + 1) // 2 + 1))
 	def next(self):
@@ -24,9 +19,21 @@ class Draw:
 		if p:
 			return self.n
 		return 1
+def result(p1, p2):
+	if p1 < p2:
+		return 2
+	elif p1 == p2:
+		return 1
+	else:
+		return 0
 n = int(input("N?: "))
+#ratings = [Rating() for _ in range(n + 1)]
 draw = Draw(n)
-print(draw.tour)
-for _ in range(n - 2):
-	draw.next()
+for _ in range(n - 1):
 	print(draw.tour)
+	draw.next()
+#for _ in range((n - 1) * 1000):
+#	for p1, p2 in draw.tour:
+#		ratings[p1].update(ratings[p2], result(p1, p2))
+#	print([r.rating for r in ratings[1:]])
+#	draw.next()
