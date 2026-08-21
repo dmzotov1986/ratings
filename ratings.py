a3317908030bcc29efe950f0506a5cc7dfe20ed7
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-22 02:55:27 +0300

    wip немножко

--- ratings.py
+++ ratings.py
@@ -7,7 +7,7 @@ class Rating:
 		other.rating -= increase
 class Draw:
 	def __init__(self, n):
-		#Нечетные неправильно работают, нужен ещё тур! Надёжнее лишний игрок, с которым отдыхают.
+		#Нечетные неправильно работают, нужен ещё тур! Надёжнее лишний игрок, с которым отдыхают. Только для исследований, некоторые дефекты.
 		self.n = n
 		self.tour = tuple((p, n + 1 - p) for p in range(1, (n + 1) // 2 + 1))
 	def next(self):
@@ -20,6 +20,7 @@ class Draw:
 			return self.n
 		return 1
 def result(p1, p2):
+	#Другая зависимость, другая жеребьёвка, другие рейтинги (для ручных сортировок).
 	if p1 < p2:
 		return 2
 	elif p1 == p2:
@@ -27,13 +28,14 @@ def result(p1, p2):
 	else:
 		return 0
 n = int(input("N?: "))
-#ratings = [Rating() for _ in range(n + 1)]
 draw = Draw(n)
-for _ in range(n - 1):
-	print(draw.tour)
-	draw.next()
-#for _ in range((n - 1) * 1000):
-#	for p1, p2 in draw.tour:
-#		ratings[p1].update(ratings[p2], result(p1, p2))
-#	print([r.rating for r in ratings[1:]])
+#for _ in range(n - 1):
+#	print(draw.tour)
 #	draw.next()
+ratings = [Rating() for _ in range(n + 1)]
+for _ in range((n - 1) * 1000):
+	for p1, p2 in draw.tour:
+		ratings[p1].update(ratings[p2], result(p1, p2))
+	print(" ".join(str(r.rating) for r in ratings[1:]))
+	draw.next()
+#Исследование в отдельном файле.
