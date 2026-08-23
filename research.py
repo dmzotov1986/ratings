dc2546e51d32d127455c1782a020b15961cdec97
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-24 01:53:15 +0300

    wip немножко.

--- research.py
+++ research.py
@@ -5,37 +5,36 @@ class Draw:
 		self.n = n
 		self.tour = tuple((p, n + 1 - p) for p in range(1, (n + 1) // 2 + 1))
 	def next(self):
-		self.tour = tuple((self.substitute(p1), self.substitute(p2)) for p1, p2 in self.tour)
-	def substitute(self, p):
-		p -= 1
-		if p > 1:
-			return p
-		if p:
-			return self.n
-		return 1
+		def substitute(p):
+			p -= 1
+			if p > 1:
+				return p
+			if p:
+				return self.n
+			return 1
+		self.tour = tuple((substitute(p1), substitute(p2)) for p1, p2 in self.tour)
 def result(p1, p2):
-	#Другая зависимость, другая жеребьёвка, другие рейтинги (для ручных сортировок).
-#	if p1 < p2:
-#		return 2
-#	elif p1 == p2:
-#		return 1
-#	else:
-#		return 0
-	if abs(p1 - p2) <= 2:
+	print((p1, p2), end = ":")
+	#Другая жеребьёвка, другие рейтинги (для ручных сортировок).
+	if abs(p1 - p2) <= 2:#одно условие и abs заменить
 		if random.choice((False, True)):
+			print(1, end = " ")
 			return 1
 	if p1 < p2:
+		print(2, end = " ")
 		return 2
+	print(0, end = " ")
 	return 0
 n = int(input("N?: "))
 t = int(input("T?: "))
 draw = Draw(n)
 ratings = [ratings.Rating() for _ in range(n + 1)]
-for _ in range((n - 1) * t):
-	print(draw.tour)
-	for p1, p2 in draw.tour:
-		ratings[p1].update(ratings[p2], result(p1, p2))
-	for rating in ratings[1:]:
-		print(rating.rating, end = " ")
-	print()
-	draw.next()
+for _ in range(t):
+	for _ in range(n - 1):
+		for p1, p2 in draw.tour:
+			ratings[p1].update(ratings[p2], result(p1, p2))
+		print()
+		for rating in ratings[1:]:
+			print(rating.rating, end = " ")
+		print()
+		draw.next()
