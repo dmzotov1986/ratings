ab9a7742c518ec6d2578756350deba618bfba6c7
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-30 03:15:59 +0300

    wip

--- ratings.py
+++ ratings.py
@@ -1,12 +1,8 @@
-class Rating:
-	def __init__(self, rating = 0.0):
-		self._rating = rating
-	@property
-	def rating(self):
-		return self._rating
-	def update(self, other, result):
-		if result < 0 or result > 2:
-			raise ValueError("Результат матча для первого игрока от 0 до 2")
-		increase = result - 2 / (10 ** ((other._rating - self._rating) / 25) + 1)
-		self._rating += increase
-		other._rating -= increase
+INITIAL_RATING = 0.0
+def update(first, second, result):
+	if result < 0 or result > 2:
+		raise ValueError("Результат матча для первого игрока от 0 до 2")
+	increase = result - 2 / (10 ** ((second - first) / 25) + 1)
+	first += increase
+	second -= increase
+	return first, second
--- research.py
+++ research.py
@@ -1,24 +1,28 @@
 import ratings, random
 def draw(n):
-	#Нечетные неправильно работают, нужен ещё тур! Надёжнее лишний игрок, с которым отдыхают.
 	n += n & 1
 	tour = tuple((p, n + 1 - p) for p in range(1, n // 2 + 1))
+	substitution = Substitution(n)
 	while True:
 		yield tour
 		if tour[0][1] == 2:
 			return
-		tour = tuple(substitute_match(*match, n) for match in tour)
-def substitute(p, n):#перевести на объект?
-	if p > 2:
-		return p - 1
-	if p == 2:
-		return n
-	return 1
-def substitute_match(p1, p2, n):
-	if p1 == 1 or p2 == 1:
-		p1, p2 = p2, p1
-	return substitute(p1, n), substitute(p2, n)
+		tour = tuple(substitution.match(*match) for match in tour)
+class Substitution:#на глобальную переменную?
+	def __init__(self, n):
+		self._n = n
+	def player(self, p):
+		if p > 2:
+			return p - 1
+		if p == 2:
+			return self._n
+		return 1
+	def match(self, p1, p2):
+		if p1 == 1 or p2 == 1:
+			p1, p2 = p2, p1
+		return self.player(p1), self.player(p2)
 def result(p1, p2):
+	#Последний игрок иногда отсутствует!
 	#Другая жеребьёвка, другие рейтинги (для ручных сортировок).
 	print((p1, p2), end = ":")
 	if abs(p1 - p2) <= 2 and random.choice((False, True)):
@@ -31,13 +35,17 @@ def result(p1, p2):
 	return result
 n = int(input("Количество участников: "))
 t = int(input("Количество круговых турниров: "))
-#Нужна подстановка место/сила!
-ratings = [ratings.Rating() for _ in range(n + 1)]
+player_ratings = [ratings.INITIAL_RATING for _ in range(n + 1)]
+place_to_rank = [rank for rank in range(1, n + 1)]
+ptr = [0]
 for _ in range(t):
+	random.shuffle(place_to_rank)
+	ptr[1:] = place_to_rank
 	for tour in draw(n):
-		for p1, p2 in tour:
-			ratings[p1].update(ratings[p2], result(p1, p2))
+		for match in tour:
+			p = tuple(ptr[p] for p in match)
+			player_ratings[p[0]], player_ratings[p[1]] = ratings.update(player_ratings[p[0]], player_ratings[p[1]], result(p[0], p[1]))
 		print()
-		for rating in ratings[1:]:
-			print(rating.rating, end = " ")
+		for rating in player_ratings[1:]:
+			print(rating, end = " ")
 		print()
