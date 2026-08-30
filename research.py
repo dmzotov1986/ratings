6f597665a56fbcf6d63696cd67a345d212d8d7a7
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-08-31 02:52:40 +0300

    wip

--- research.py
+++ research.py
@@ -1,13 +1,14 @@
 import ratings, random
 def draw(n):
 	n += n & 1
-	tour = tuple((p, n + 1 - p) for p in range(1, n // 2 + 1))
+	tour = ((p, n + 1 - p) for p in range(1, n // 2 + 1))
 	substitution = Substitution(n)
 	while True:
 		yield tour
+		#Проверять n
 		if tour[0][1] == 2:
 			return
-		tour = tuple(substitution.match(*match) for match in tour)
+		tour = (substitution.match(*match) for match in tour)
 class Substitution:#на глобальную переменную?
 	def __init__(self, n):
 		self._n = n
@@ -31,21 +32,23 @@ def result(p1, p2):
 		result = 2
 	else:
 		result = 0
+	if not random.getrandbits(4):
+		result = 2 - result
 	print(result, end = " ")
 	return result
 n = int(input("Количество участников: "))
 t = int(input("Количество круговых турниров: "))
-player_ratings = [ratings.INITIAL_RATING for _ in range(n + 1)]
-place_to_rank = [rank for rank in range(1, n + 1)]
-ptr = [0]
+rank_to_rating = [ratings.INITIAL_RATING for _ in range(n + 1)]
+place_to_rank = [rank for rank in range(n + 1)]
 for _ in range(t):
-	random.shuffle(place_to_rank)
-	ptr[1:] = place_to_rank
+	place_to_rank1 = place_to_rank[1:]
+	random.shuffle(place_to_rank1)
+	place_to_rank[1:] = place_to_rank1
 	for tour in draw(n):
 		for match in tour:
-			p = tuple(ptr[p] for p in match)
-			player_ratings[p[0]], player_ratings[p[1]] = ratings.update(player_ratings[p[0]], player_ratings[p[1]], result(p[0], p[1]))
+			p1, p2 = (place_to_rank[p] for p in match)
+			rank_to_rating[p1], rank_to_rating[p2] = ratings.update(rank_to_rating[p1], rank_to_rating[p2], result(p1, p2))
 		print()
-		for rating in player_ratings[1:]:
+		for rating in rank_to_rating[1:]:
 			print(rating, end = " ")
 		print()
