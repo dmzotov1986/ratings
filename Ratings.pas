257edc061855757d43aea441480b1b26eb628604
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-02 06:09:15 +0300

    wip

--- Ratings.pas
+++ Ratings.pas
@@ -2,10 +2,10 @@ unit Ratings;
 interface
 const InitialRating = 0.0;
 type Result = 0..2;
-procedure Update(var First, Second: Real; const Result: Result);
+procedure Update(var First, Second: Real; {const} Result: Result);
 implementation
-procedure Update(var First, Second: Real; const Result: Result);
+procedure Update(var First, Second: Real; {const} Result: Result);
 begin
-	
+    WriteLn('Test!')
 end;
 end.
