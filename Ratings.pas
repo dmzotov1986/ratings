aa76de9594448d2b8bcdc15edbe490585c4a724c
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-03 02:32:41 +0300

    wip

--- Ratings.pas
+++ Ratings.pas
@@ -2,10 +2,14 @@ unit Ratings;
 interface
 const InitialRating = 0.0;
 type Result = 0..2;
-procedure Update(var First, Second: Real; {const} Result: Result);
+procedure Update(var First, Second: Real; Result: Result);
 implementation
-procedure Update(var First, Second: Real; {const} Result: Result);
+const C = ln(10) / 25;{unit Math!}
+procedure Update(var First, Second: Real; Result: Result);
+var Increase: Real;
 begin
-    WriteLn('Test!')
+    Increase := Result - 2 / (Exp(C * (Second - First)) + 1);
+    Inc(First, Increase);
+    Dec(Second, Increase)
 end;
 end.
--- ratings.py
+++
@@ -1,8 +0,0 @@
-INITIAL_RATING = 0.0
-def update(first, second, result):
-	if result < 0 or result > 2:
-		raise ValueError("Результат матча для первого игрока от 0 до 2")
-	increase = result - 2 / (10 ** ((second - first) / 25) + 1)
-	first += increase
-	second -= increase
-	return first, second
