982cc47f7e89038061e389cbcec1dc1a85e9ac1a
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-04 04:19:38 +0300

    wip

--- research.py
+++ research.py
@@ -32,25 +32,23 @@ def result(p1, p2):
 		result = 2
 	else:
 		result = 0
-	#есть оптимальнее ветвление?
 	if not random.getrandbits(4):
 		result = 2 - result
 	print(result, end = " ")
 	return result
 n = int(input("Количество участников: "))
 t = int(input("Количество круговых турниров: "))
-rank_to_rating = [ratings.INITIAL_RATING for _ in range(n + 1)]#по другому
-#чаще смотрится рейтинг, чем настоящее место?
-place_to_rank = [rank for rank in range(n + 1)]#по другому и от 0 все
+rank_to_rating = dict.fromkeys(range(1, n + 1), ratings.INITIAL_RATING)
+place_to_rank = dict(enumerate(range(1, n + 1), 1))
 for _ in range(t):
-	place_to_rank1 = place_to_rank[1:]
-	random.shuffle(place_to_rank1)
-	place_to_rank[1:] = place_to_rank1
+	ranks = place_to_rank.values()
+	random.shuffle(ranks)
+	place_to_rank = dict(enumerate(ranks, 1))
 	for tour in draw(n):
 		for match in tour:
 			p1, p2 = (place_to_rank[p] for p in match)
 			rank_to_rating[p1], rank_to_rating[p2] = ratings.update(rank_to_rating[p1], rank_to_rating[p2], result(p1, p2))
 		print()
-		for rating in rank_to_rating[1:]:
+		for rating in rank_to_rating:
 			print(rating, end = " ")
 		print()
