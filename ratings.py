e9bfbdb7c5415ac643cc2539ca0e50e5d3841ccc
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-18 04:26:34 +0300

    Архитектурные изменения

--- ratings.py
+++ ratings.py
@@ -2,40 +2,47 @@ for module in ("random", "itertools", "operator"):
 	exec(f"from {module} import *")
 INITIAL_RATING = 0.0
 def update():
-	res = result(match)
-	for rank, score in zip(match, (res, 2 - res)):
+	res = result()
+	for rank, score in zip(match_r, (res, 2 - res)):
 		rank_to_score[rank] += score
-	increase = res - 2 / (10 ** ((sub(*map(rank_to_rating.get, reversed(match)))) / 25) + 1)
-	for rank, increase in zip(match, (increase, -increase)):
+	ratings = []
+	for rank in match_r:
+		ratings.append(rank_to_rating[rank])
+	r1, r2 = ratings
+	increase = res - 2 / (10 ** ((r2 - r1) / 25) + 1)
+	for rank, increase in zip(match_r, (increase, -increase)):
 		rank_to_rating[rank] += increase
-def table_of_matches(n):
-	tour = tuple(takewhile(lambda match: lt(*match), zip(count(1), count(n, -1))))
-	def substitute_player(p):
-		match p:
-			case _ if p > 2:
-				return p - 1
-			case 2:
-				return n
-			case _:
-				return 1
-	def substitute_match(match):
-		if 1 in match:
-			match = reversed(match)
-		return tuple(map(substitute_player, match))
+def table_of_matches():
+	#Без генераторов
+	tour = []
+	for p1, p2 in zip(count(0), count(n, -1)):
+		if p1 >= p2:
+			break
+		tour.append([p1, p2])
 	def tour_supplier():
-		nonlocal tour
 		while True:
 			yield tour
-			tour = tuple(map(substitute_match, tour))
-	return islice(tour_supplier(), n - 1)
-def result(match):
+			for match in tour:
+				if 0 in match:
+					match.reverse()
+				for i, p in enumerate(match):
+					p -= 1
+					if p <= 0:
+						if p == 0:
+							p = n
+						else:
+							p = 0
+					match[i] = p
+	return islice(tour_supplier(), n)
+def result():
 	#Другая жеребьёвка, другие рейтинги, для ручных сортировок.
-	print(match, end = ":")
-	distance = abs(sub(*match))
+	print(match_r, end = ":")
+	p1, p2 = match_r
+	distance = abs(p1 - p2)
 	match distance:
 		case 0 | _ if random() < 1 / (distance ** (1 / 3) + 1):
 			result = 1
-		case _ if lt(*match):
+		case _ if p1 < p2:
 			result = 2
 		case _:
 			result = 0
@@ -44,21 +51,22 @@ def result(match):
 	print(result, end = " ")
 	return result
 n = int(input("Количество участников: "))
-rank_to_rating = dict.fromkeys(islice(count(1), n), INITIAL_RATING)
-rank_to_score = dict.fromkeys(islice(count(1), n), 0)
+rank_to_rating = [INITIAL_RATING] * n
+rank_to_score = [0] * n
 odd = n & 1
 n += odd
+place_to_rank = list(range(n))
+n -= 1
 t = int(input("Количество круговых турниров: "))
-place_to_rank = dict(enumerate(islice(count(1), n), 1))
 for _ in range(t):
-	ranks = list(place_to_rank.values())
-	shuffle(ranks)
-	place_to_rank = dict(enumerate(ranks, 1))
-	for tour in table_of_matches(n):
+	shuffle(place_to_rank)
+	for tour in table_of_matches():
 		for match in tour:
-			match = tuple(map(place_to_rank.get, match))
-			if not(odd and n in match):
+			match_r = []
+			for p in match:
+				match_r.append(place_to_rank[p])
+			if not(odd and n in match_r):
 				update()
 		print()
-		print(sorted(rank_to_rating.items(), key=itemgetter(1), reverse=True))
-		print(sorted(rank_to_score.items(), key=itemgetter(1), reverse=True))
+		print(rank_to_rating)
+		print(rank_to_score)
