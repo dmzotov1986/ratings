bc497f06cd5d88f1ead4739ab76fd40cbf898e1b
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-18 12:02:53 +0300

    refactoring

--- ratings.py
+++ ratings.py
@@ -1,17 +1,6 @@
 for module in ("random", "itertools", "operator"):
 	exec(f"from {module} import *")
 INITIAL_RATING = 0.0
-def update():
-	res = result()
-	for rank, score in zip(match_r, (res, 2 - res)):
-		rank_to_score[rank] += score
-	ratings = []
-	for rank in match_r:
-		ratings.append(rank_to_rating[rank])
-	r1, r2 = ratings
-	increase = res - 2 / (10 ** ((r2 - r1) / 25) + 1)
-	for rank, increase in zip(match_r, (increase, -increase)):
-		rank_to_rating[rank] += increase
 def table_of_matches():
 	#Без генераторов
 	tour = []
@@ -34,22 +23,6 @@ def table_of_matches():
 							p = 0
 					match[i] = p
 	return islice(tour_supplier(), n)
-def result():
-	#Другая жеребьёвка, другие рейтинги, для ручных сортировок.
-	print(match_r, end = ":")
-	p1, p2 = match_r
-	distance = abs(p1 - p2)
-	match distance:
-		case 0 | _ if random() < 1 / (distance ** (1 / 3) + 1):
-			result = 1
-		case _ if p1 < p2:
-			result = 2
-		case _:
-			result = 0
-	if random() < 1 / (distance + 15):
-		result = 2 - result
-	print(result, end = " ")
-	return result
 n = int(input("Количество участников: "))
 rank_to_rating = [INITIAL_RATING] * n
 rank_to_score = [0] * n
@@ -66,7 +39,29 @@ for _ in range(t):
 			for p in match:
 				match_r.append(place_to_rank[p])
 			if not(odd and n in match_r):
-				update()
+				#Другая жеребьёвка, другие рейтинги, для ручных сортировок.
+				print(match_r, end = ":")
+				p1, p2 = match_r
+				distance = abs(p1 - p2)
+				match distance:
+					case 0 | _ if random() < 1 / (distance ** (1 / 3) + 1):
+						result = 1
+					case _ if p1 < p2:
+						result = 2
+					case _:
+						result = 0
+				if random() < 1 / (distance + 15):
+					result = 2 - result
+				print(result, end = " ")
+				for rank, score in zip(match_r, (result, 2 - result)):
+					rank_to_score[rank] += score
+				ratings = []
+				for rank in match_r:
+					ratings.append(rank_to_rating[rank])
+				r1, r2 = ratings
+				increase = result - 2 / (10 ** ((r2 - r1) / 25) + 1)
+				for rank, increase in zip(match_r, (increase, -increase)):
+					rank_to_rating[rank] += increase
 		print()
 		print(rank_to_rating)
 		print(rank_to_score)
