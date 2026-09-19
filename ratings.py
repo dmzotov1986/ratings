1baf3f6a28c1a33d6a29bee7b713ac4fa83a4ff8
Дмитрий Зотов <dmzotov1986@gmail.com>
2026-09-20 02:04:32 +0300

    refactoring

--- ratings.py
+++ ratings.py
@@ -7,9 +7,10 @@ def table_of_matches():
 	for i in range(m):
 		tour[i] = [None, None]
 	for i, p1, p2 in zip(range(m), count(0), count(n, -1)):
+		match_places = tour[i]
 		p = p2
 		for j in count(1, -1):
-			tour[i][j] = p
+			match_places[j] = p
 			if not j:
 				break
 			p = p1
@@ -17,18 +18,21 @@ def table_of_matches():
 		while True:
 			yield tour
 			for i in range(m):
-				match = tour[i]
-				if 0 in match:
-					match.reverse()
+				match_places = tour[i]
+				for i in range(2):
+					if not match_places[i]:
+						match_places[i] = match_places[1 - i]
+						match_places[1 - i] = 0
+						break
 				for j in range(2):
-					p = match[j]
+					p = match_places[j]
 					p -= 1
 					if p <= 0:
 						if p == 0:
 							p = n
 						else:
 							p = 0
-					match[j] = p
+					match_places[j] = p
 	return islice(tour_supplier(), n)
 n = int(input("Количество участников: "))
 rank_to_rating = [INITIAL_RATING] * n
@@ -40,38 +44,59 @@ for i in range(n):
 	place_to_rank[i] = i
 m = n // 2
 n -= 1
+#Линейное хранение тура?
 t = int(input("Количество круговых турниров: "))
 for _ in range(t):
 	shuffle(place_to_rank)
 	for tour in table_of_matches():
-		for match in tour:
-			match_r = [None] * 2
-			for i, p in enumerate(match):
-				match_r[i] = place_to_rank[p]
-			if not(odd and n in match_r):
+		i = 0
+		while True:
+			match_places = tour[i]
+			match_ranks = [None] * 2
+			for j in range(2):
+				match_ranks[j] = place_to_rank[match_places[j]]
+			if odd:
+				for j in range(2):
+					if match_ranks[j] == n:
+						update = False
+						break
+				else:
+					update = True
+			else:
+				update = True
+			if update:
 				#Другая жеребьёвка, другие рейтинги, для ручных сортировок.
-				print(match_r, end = ":")
-				p1, p2 = match_r
-				distance = abs(p1 - p2)
-				match distance:
-					case 0 | _ if random() < 1 / (distance ** (1 / 3) + 1):
-						result = 1
-					case _ if p1 < p2:
-						result = 2
-					case _:
-						result = 0
+				for j in count(0):
+					print(match_ranks[j], end = "")
+					if j == 1:
+						break
+					print(end = "-")
+				for j in range(2):
+					exec(f"p{j + 1} = match_ranks[j]")
+				diff = p1 - p2
+				distance = abs(diff)
+				if random() < 1 / (distance ** (1 / 3) + 1):
+					result = 1
+				elif diff < 0:
+					result = 2
+				else:
+					result = 0
 				if random() < 1 / (distance + 15):
 					result = 2 - result
-				print(result, end = " ")
-				for rank, score in zip(match_r, (result, 2 - result)):
+				print(" :", result, end = "")
+				for rank, score in zip(match_ranks, (result, 2 - result)):
 					rank_to_score[rank] += score
 				ratings = []
-				for rank in match_r:
+				for rank in match_ranks:
 					ratings.append(rank_to_rating[rank])
 				r1, r2 = ratings
 				increase = result - 2 / (10 ** ((r2 - r1) / 25) + 1)
-				for rank, increase in zip(match_r, (increase, -increase)):
+				for rank, increase in zip(match_ranks, (increase, -increase)):
 					rank_to_rating[rank] += increase
+			i += 1
+			if i >= m:
+				break
+			print(end = " ")
 		print()
 		print(rank_to_rating)
 		print(rank_to_score)
